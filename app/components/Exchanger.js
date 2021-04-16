b4702383b81190fa389f59c630ccc78b089360a2
Cristian Macedo <37914402+CristianMacedo@users.noreply.github.com>
2021-04-16 01:28:36 -0300

    feat: implement api usage logic

--- app/components/Exchanger.js
+++ app/components/Exchanger.js
@@ -1,48 +1,87 @@
 import React from "react";
 
+import Card from "./Card";
+
 import Currency from "./Currency";
+
+import { fetchCurrentPrice, fetchHistoricalPrice } from "../utils/api";
 
 export default class Exchanger extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
+            cashCode: "USD",
+            cryptoCode: "BTC",
             cashValue: 0,
             cryptoValue: 0,
-            cryptoPrice: 63223.7683,
+            currentCryptoPrice: 0,
+            historicalDate: "2015-01-01",
+            historicalCryptoPrice: 0,
         };
 
-        this.handleCashChange = this.handleCashChange.bind(this);
-        this.handleCryptoChange = this.handleCryptoChange.bind(this);
+        this.handleCashValueChange = this.handleCashValueChange.bind(this);
+        this.handleCryptoValueChange = this.handleCryptoValueChange.bind(this);
     }
 
-    handleCashChange(e) {
-        this.setState((state) => ({
+    componentDidMount() {
+        fetchCurrentPrice(this.state.cashCode).then((data) => {
+            const currentCryptoPrice =
+                data["bpi"][this.state.cashCode]["rate_float"];
+            this.setState({ currentCryptoPrice });
+        });
+        fetchHistoricalPrice(this.state.historicalDate).then((data) => {
+            const historicalCryptoPrice =
+                data["bpi"][this.state.historicalDate];
+            this.setState({ historicalCryptoPrice });
+        });
+    }
+
+    handleCashValueChange(e) {
+        this.setState((currentState) => ({
             cashValue: e.target.value,
-            cryptoValue: (e.target.value / state.cryptoPrice).toFixed(8),
+            cryptoValue: (
+                e.target.value / currentState.historicalCryptoPrice
+            ).toFixed(8),
         }));
     }
 
-    handleCryptoChange(e) {
-        this.setState((state) => ({
-            cryptoValue: e.target.value.toFixed(8),
-            cashValue: e.target.value * state.cryptoPrice,
+    handleCryptoValueChange(e) {
+        this.setState((currentState) => ({
+            cryptoValue: e.target.value,
+            cashValue: (
+                e.target.value * currentState.historicalCryptoPrice
+            ).toFixed(2),
         }));
     }
 
     render() {
         return (
             <React.Fragment>
-                <Currency
-                    name="USD"
-                    value={this.state.cashValue}
-                    onValueChange={this.handleCashChange}
-                />
-                <Currency
-                    name="BTC"
-                    value={this.state.cryptoValue}
-                    onValueChange={this.handleCashChange}
-                />
+                <Card>
+                    <p>If you had bought</p>
+                    <Currency
+                        name={this.state.cashCode}
+                        value={this.state.cashValue}
+                        onValueChange={this.handleCashValueChange}
+                    />
+                    <p className="mt-1">worth of</p>
+                    <Currency
+                        name={this.state.cryptoCode}
+                        value={this.state.cryptoValue}
+                        onValueChange={this.handleCryptoValueChange}
+                    />
+                    <p className="mt-1">in {this.state.historicalDate}.</p>
+                </Card>
+                <Card>
+                    <p>Today you would have:</p>
+                    <h2 className="text-center font-size-bg">
+                        {`${this.state.cashCode} ${(
+                            this.state.cryptoValue *
+                            this.state.currentCryptoPrice
+                        ).toFixed(2)}`}
+                    </h2>
+                </Card>
             </React.Fragment>
         );
     }
