9317540fb33b12b8b7a716da3993e6c0e99b4594
Cristian Macedo <37914402+CristianMacedo@users.noreply.github.com>
2021-05-18 00:02:14 -0300

    feat: add equivalent padding to card component content

--- app/components/Card.js
+++ app/components/Card.js
@@ -5,11 +5,7 @@ import PropTypes from "prop-types";
  * Card-styled container.
  */
 export default function Card({ children }) {
-    return (
-        <div className="container card mb-4">
-            <div className="card-body">{[children]}</div>
-        </div>
-    );
+    return <div className="container card mb-4 p-4">{[children]}</div>;
     // return <div className="container card center p-1 mb-1">{[children]}</div>;
 }
 
