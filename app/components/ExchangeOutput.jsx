019bec745b7feead2e0748bbfe59629a9fd03d94
Cristian Macedo <cristian.silvadk22@gmail.com>
2021-10-05 13:17:53 -0300

    feat: allow decimals on output value

--- app/components/ExchangeOutput.jsx
+++ app/components/ExchangeOutput.jsx
@@ -34,6 +34,7 @@ export default function ExchangeOutput({
             duration={1}
             start={previousResult}
             end={result}
+            decimals={2}
             formattingFn={formatter.format}
           />
         )}
